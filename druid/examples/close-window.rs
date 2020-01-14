35b9d1ecac50b272c4788f21ab1e7c6fce059964
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2020-01-14 23:34:36 +0100

    Add broken main

--- druid/examples/close-window.rs
+++ druid/examples/close-window.rs
@@ -1,23 +1,50 @@
+use std::{thread, time};
 use druid::widget::{Button, Flex};
 use druid::{AppLauncher, Widget, WindowDesc};
 
+// Clicking a button should close the window
+
+//// This works like expected
+//fn main () {
+//    let window = WindowDesc::new(build_widget);
+//    AppLauncher::with_window(window)
+//        .use_simple_logger()
+//        .launch(0_u32)
+//        .expect("launch failed");
+//
+//    dbg!("GUI killed");
+//}
+
+// This not
 fn main () {
     dbg!("launch_gui");
 
-    let window = WindowDesc::new(build_widget);
-    AppLauncher::with_window(window)
-        .use_simple_logger()
-        .launch(0_u32)
-        .expect("launch failed");
+    let mut should_gui = true;
+    loop {
+        if should_gui == true {
+            dbg!("INSIDE");
+            should_gui = false;
 
-    dbg!("GUI killed");
+            let window = WindowDesc::new(build_widget);
+            AppLauncher::with_window(window)
+                .use_simple_logger()
+                .launch(0_u32)
+                .expect("launch failed");
+
+            dbg!("GUI killed");
+
+        }
+        let sleep_time = time::Duration::from_millis(100);
+        thread::sleep(sleep_time);
+    }
+
 }
 
 fn build_widget() -> impl Widget<u32> {
     Flex::column()
         .with_child(
             Button::sized(
-                "Copy",
+                "Close",
                 |evt_ctx , data: _, _env| {
                     dbg!(&data);
                     evt_ctx.window().close();
